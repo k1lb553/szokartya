8531626d1420ac4d925f94522de0de9e1c0617a6
k1lb553 <66701524+k1lb553@users.noreply.github.com>
2026-02-10 14:12:43 +0100

    Update __init__.py

--- szokartyaV1/app/main/__init__.py
+++ szokartyaV1/app/main/__init__.py
@@ -7,6 +7,6 @@ from flask_login import LoginManager
 main_bp = Blueprint('main', __name__)
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/cgpt_coll'
-app.secret_key = 'hpAVBF572FOe6HLBsoZTxnapSNhO3L8T'
+app.secret_key = '#'
 mongo = PyMongo(app)
 login_manager = LoginManager(app)
